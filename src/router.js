e60ce6ec5e9ba0e2d0d330b4511ff918dfe97b04
ytj <ytj1009655887@163.com>
2019-09-16 16:06:54 +0800

    初始化代码库

--- src/router.js
+++ src/router.js
@@ -10,6 +10,14 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'index',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import( /* webpackChunkName: "about" */ './views/Index.vue')
+    },
+    {
+      path: '/home',
       name: 'home',
       component: Home
     },
---
+++ src/views/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="router-link-container">
+    <router-link to="/home" class="router-link">home</router-link>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'index',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+<style lang="less" >
+.router-link-container{
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    .router-link{
+        color: #666;
+        line-height: 150%;
+        font-size: 15px;
+        
+    }
+}
+    
+</style>
